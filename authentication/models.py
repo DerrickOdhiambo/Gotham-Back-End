d1423c77e946c3d86b065e98682ddcda6925f19b
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-11-04 12:06:31 +0300

    deploy to heroku

---
+++ authentication/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+# from django.conf import settings
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
+# from rest_framework.authtoken.models import Token
+# from django.contrib.auth.models import User
+
+
+# @receiver(post_save, sender=User)
+# def create_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Token.objects.create(user=instance)
