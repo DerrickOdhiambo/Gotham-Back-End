170d810c32d12ad58927b613af70e4febd7cd033
msyoki <msyokimutua@gmail.com>
2020-11-05 15:59:20 +0300

    add  get movie url

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import RegisterView, LoginView,MovieList
+from .views import RegisterView, LoginView,MovieList,moviedetails
+from . import views
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
     path('movies/', MovieList.as_view()),
+    path('movies/<int:pk>/', views.moviedetails),
 ]
